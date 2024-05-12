672a61b1e7a96bcf77a01c5753fe0a0fd14f5448
geo318 <geo.lomidze@gmail.com>
2024-05-12 13:17:09 +0400

    chore: remove temp stored original file

--- utils/index.ts
+++ utils/index.ts
@@ -2,6 +2,8 @@ export { default as resizeImage } from './resize'
 import { resizeImage } from '/utils'
 import crypto from 'crypto'
 import env from '/env'
+import getSize from 'image-size'
+import fs from 'fs'
 
 export const addNewImage = (image: Express.Multer.File, userFolder: string) => {
   let [name, ext] = image.originalname.trim().replace(/\s/g, '-').split(/\./)
@@ -9,13 +11,21 @@ export const addNewImage = (image: Express.Multer.File, userFolder: string) => {
 
   const imageToResize = new resizeImage(
     image,
-    'assets/images',
+    `assets/images/${userFolder}`,
     'webp',
     undefined,
     name
   )
-  imageToResize.save(userFolder, 1500)
+  const { width, height } = getSize(image.path)
+  const imgWidth = width && width <= 2000 ? width : undefined
+  const imgHeight = height && height <= 2000 ? height : undefined
+
+  imageToResize.save('full', imgWidth, imgHeight)
+  imageToResize.save('thumb', 10)
+
   const path = imageToResize.fullFilePath()
+  fs.unlinkSync(`./${image.path}`)
+
   return path
 }
 
